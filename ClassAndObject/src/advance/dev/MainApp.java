ef91f10141f7f1a36d8adc920da712209d317255
hoangdiver <144077699+hoangdiver@users.noreply.github.com>
2024-06-21 14:39:56 +0700

    Add files via upload

---
+++ ClassAndObject/src/advance/dev/MainApp.java
@@ -0,0 +1,16 @@
+package advance.dev;
+import java.util.Scanner;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Person p = new Person();
+		p.name = "hoang";
+        p.age = 19;
+        p.dtb = 9;
+        p.Address = "Hue";
+        System.out.println(p.name+"\n"+p.age+"\n"+p.dtb+"\n"+p.Address);
+	}
+
+}
